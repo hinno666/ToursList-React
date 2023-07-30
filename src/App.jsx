0f2466d279a129c581f4916e1ad60a6f6fe5f76b
Andrey <luffytaroyo@gmail.com>
2023-07-30 15:04:51 +0300

    added loader, delete functiona, readmore functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useEffect, useState } from "react";
 import { Tours } from "./Tours";
+import { Loading } from "./Loading";
 const API_LINK = 'https://course-api.com/react-tours-project';
 
 const App = () => {
   const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+  const deleteTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id != id)
+    setTours(newTours);
+  }
 
   const getTours = async () => {
     try {
       const response = await fetch(API_LINK)
       const tours = await response.json()
+      setLoading(false)
       setTours(tours);
       console.log(tours);
     } catch (error) {
@@ -20,11 +28,17 @@ const App = () => {
     getTours()
   }, [])
 
-
+  if(loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
 
   return (
     <main>
-      <Tours />
+      <Tours tours={tours} deleteTour={deleteTour}/>
     </main>
   )
 };
