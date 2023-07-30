a340cc026e2a6dc8edc843d547ba034a6ab79e46
Andrey <luffytaroyo@gmail.com>
2023-07-30 14:32:09 +0300

    added components

---
+++ src/Tour.jsx
@@ -0,0 +1,15 @@
+export const Tour = () => {
+    return (
+        <article className="single-tour">
+            <img src="" alt="" className="img"/>
+            <span className="tour-price">123 $</span>
+            <div className="tour-info">
+                <h4>Name of tour</h4>
+                <p>some text..
+                    <button className="info-btn">read more</button>
+                    <button className='delete-btn btn-block btn'>Dont interesting</button>
+                </p>
+            </div>
+        </article>
+    )
+}
