9a8915b4360f46ee38eff48cca5f04dca62ef851
Andrey <luffytaroyo@gmail.com>
2023-07-30 14:14:28 +0300

    start working with project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+const API_LINK = 'https://course-api.com/react-tours-project';
+
+
+
+const App = () => {
+  return (
+    <h2></h2>
+  )
+};
+export default App;
