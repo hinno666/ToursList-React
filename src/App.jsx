6f81dc92f052b33aee70db09b587dfd0613421a5
Andrey <luffytaroyo@gmail.com>
2023-07-31 21:25:44 +0300

    added refetch condition

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ const API_LINK = 'https://course-api.com/react-tours-project';
 
 const App = () => {
   const [tours, setTours] = useState([]);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
 
   const deleteTour = (id) => {
     const newTours = tours.filter((tour) => tour.id != id)
-    setTours(newTours);
+    setTours(newTours);slick
   }
 
   const getTours = async () => {
     try {
+      setLoading(true);
       const response = await fetch(API_LINK)
       const tours = await response.json()
       setLoading(false)
       setTours(tours);
-      console.log(tours);
     } catch (error) {
       console.log(error);
     }
@@ -32,6 +32,17 @@ const App = () => {
     return (
       <main>
         <Loading />
+      </main>
+    )
+  }
+
+  if(tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button type="button" style={{marginTop: '2rem'}} className="btn" onClick={() => getTours()}>refresh</button>
+        </div>
       </main>
     )
   }
